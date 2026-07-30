1a1fe3718e3206e282f991283dcec603941c32e2
isael caballero <isaelcaballero@MacBook-Air-de-isael.local>
2026-07-30 10:54:37 -0600

    chore: sq w components commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { AuthProvider } from './contexts/Auth.context.jsx';
-import { LanguageProvider } from './contexts/Language.context.jsx';
-import { UIProvider } from './contexts/UI.context.jsx';
+
+import { AuthProvider } from '@/contexts/Auth.context.jsx';
+import { LanguageProvider } from '@/contexts/Language.context.jsx';
+import { UIProvider } from '@/contexts/UI.context.jsx';
+
 import App from './App.jsx';
 import './index.css';
 
