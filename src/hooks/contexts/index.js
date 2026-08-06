0fad71a6c9bc4d326b3547efa76d6988e434d437
Isael Caballero <inisaelcaballero@gmail.com>
2026-08-05 18:06:50 -0600

    feat(hooks): add new how for escape key

--- src/hooks/contexts/index.js
+++ src/hooks/contexts/index.js
@@ -1,9 +1,11 @@
 import { useAuth } from './useAuth';
 import { useLanguage } from './useLanguage';
+import { useEscapeKey } from './useEscapeKey';
 import { useUI } from './useUI';
 
 export {
   useAuth,
   useLanguage,
+  useEscapeKey,
   useUI
 };
